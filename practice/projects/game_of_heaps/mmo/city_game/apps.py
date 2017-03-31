6e6c5ced1b940b705fbcc06206f0dc51a8cc2df7
Euphe <euphetar@gmail.com>
2017-03-31 07:42:32 +0300

    delete project - useless

--- practice/projects/game_of_heaps/mmo/city_game/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CityGameConfig(AppConfig):
-    name = 'city_game'
-
-    def ready(self):
-    	from .tasks import execute_timers
-    	execute_timers(repeat=10)
